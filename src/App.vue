df3e0d838edb0473ff106469146366b8fbeb3df0
newstart0514 <1418591636@qq.com>
2024-05-20 16:08:46 +0800

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -11,8 +7,59 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-button type="primary" round @click="handleOpen">点我打开弹窗</el-button>
+  <!-- 
+    @success: 成功的回调函数 
+    @fail: 失败的回调函数
+    @close: ** 必传，请注意需要 showDialog.value = false **;
+    imageList：图片列表，可以自定义图片进行随机展示，需要传入自定义图片列表，不传为使用随机图片
+    title：弹窗的标题
+    content：弹窗内容
+    chanelWidth: 滑动轨道的长度
+    width: dialog的长度
+    imgSize：图片长度
+   -->
+  <verify-dialog
+    v-model="showDialog"
+    @success="handleSuccess" 
+    @fail="handleFail"
+    @close="handleClose" 
+    :imageList="imageList" 
+    title="我是标题"
+    content="我是内容"
+    :chanelWidth="80"
+    :width="400"
+    :imgSize="100"
+    ></verify-dialog>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import verifyDialog from './components/verifyDialog.vue'
+
+const showDialog = ref(false);
+
+const imageList = ref([
+  'https://s11.ax1x.com/2023/03/22/ppd0aOx.jpg',
+  'https://s21.ax1x.com/2024/05/02/pkkQFsg.png'
+])
+
+const handleSuccess = (data) => {
+  console.log('成功的回调', data);
+}
+const handleFail = (data) => {
+  console.log('失败的回调', data);
+}
+
+const handleOpen = () => {
+  showDialog.value = true;
+}
+
+const handleClose = (data) => {
+  showDialog.value = false;
+}
+
+</script>
 
 <style scoped>
 .logo {
--- src/components/verifyDialog.vue
+++ src/components/verifyDialog.vue
@@ -1,2 +1,106 @@
 <template>
+  <el-dialog
+    v-model="show"
+    :title="props.title"
+    :width="props.width"
+    center
+    close="handleClose"
+  >
+    <div class="container">
+      <div style="margin-bottom: 1rem;font-weight: 600;">{{ props.content }}</div>
+      <div>
+        <el-avatar :size="props.imgSize" :src="imageUrl" :style="{ transform: `rotate(${angle + sliderValue}deg)` }" />
+      </div>
+    </div>
+    <template #footer>
+      <div :style="{ width: `${props.chanelWidth}%`, marginLeft: `${excludeDistance}%` }">
+        <el-slider v-model="sliderValue" :show-tooltip="false" :min="0" :max="360" @change="handleChange" />
+      </div>
+    </template>
+  </el-dialog>
 </template>
+
+<script setup>
+import { onMounted, ref, defineProps, defineEmits  } from 'vue'
+
+const props = defineProps({
+  value: {
+    type: Boolean,
+    default: false,
+  },
+  imageList: {
+    type: Array,
+    default: []
+  },
+  title: {
+    type: String,
+    default: '人机验证'
+  },
+  chanelWidth: {
+    type: Number,
+    default: 70
+  },
+  width: {
+    type: Number,
+    default: 500
+  },
+  imgSize: {
+    type: Number,
+    default: 150
+  },
+  content: {
+    type: String,
+    default: '请将图片旋转至正确向上'
+  }
+});
+const emit = defineEmits();
+
+const show = ref(true);
+const sliderValue = ref(0)
+const angle = ref(0)
+const imageUrl = ref('')
+const excludeDistance = ref(0);
+
+const handleChange = (value) => {
+  const sum = sliderValue.value + angle.value
+  // 设置为5，容许用户有10度的偏差，防止需要精确到具体
+  if (sum % 360 < 10 || sum % 360 > 350) {
+    console.log('验证成功')
+    emit('success', '验证成功')
+  } else {
+    console.log('验证失败')
+    emit('fail', '验证失败')
+  }
+  sliderValue.value = 0
+}
+const getRandomStringFromArray = (stringArray) => {
+  if (!Array.isArray(stringArray)) {
+    console.error('字符串数组非法')
+    return 'https://picsum.photos/200/300'
+  }
+  if (stringArray.length === 0) {
+    // 没有传递数组，使用随机图片生成
+    return 'https://picsum.photos/200/300'
+  }
+  const randomIndex = Math.floor(Math.random() * stringArray.length);
+  return stringArray[randomIndex]
+}
+const handleClose = () => {
+  emit('close', '关闭弹窗')
+}
+
+onMounted(() => {
+  angle.value = Math.floor(Math.random() * 360)
+  imageUrl.value = getRandomStringFromArray(props.imageList);
+  excludeDistance.value = Math.floor((100 - props.chanelWidth) / 2);
+})
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
